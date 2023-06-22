5c0d434f18202ec649f83081ae6f98ba7912a768
wiktrek <wiktor.janiszewski10@gmail.com>
2023-06-22 14:34:42 +0200

    getKey works

--- src/app/api/[file]/page.tsx
+++ src/app/api/[file]/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 export default async function Page({ params }: { params: { file: string } }) {
   const [input, setInput] = useState('');
+  const [prevkey, setPrevkey] = useState('');
   const { file } = params;
   const path = file.replaceAll('%3A', ':').replaceAll('%26', '/');
 
@@ -12,7 +13,18 @@ export default async function Page({ params }: { params: { file: string } }) {
     })
   );
   function writeFile(path: string, text: string) {
+    console.log('a' + path + text);
+    // path: string, text: string
     // invoke('write_file', { path: path, text: text})
+  }
+  function getKey(event: React.KeyboardEvent<HTMLTextAreaElement>) {
+    const key = event.key;
+    if (key === 's' && prevkey === 'Control') {
+      writeFile(path, event.currentTarget.value);
+    }
+    setPrevkey(key);
+
+    console.log(event.currentTarget.value + 'key: ' + event.key);
   }
   // useEffect(() => {
   //   const keyDownHandler = (e: any) => console.log(`You pressed ${e.code}.`);
@@ -27,7 +39,13 @@ export default async function Page({ params }: { params: { file: string } }) {
     <>
       <div>
         <p>file: {path}</p>
-        <textarea className=" bg-black" rows={50} cols={220}>
+        <textarea
+          className=" bg-black"
+          rows={50}
+          cols={220}
+          id="text"
+          onKeyDown={getKey}
+        >
           {input}
         </textarea>
       </div>
